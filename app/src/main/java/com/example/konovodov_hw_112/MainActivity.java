9115be0f1ed254613c526cf4ef6d36b64990bd93
VladimirKonovodov <kon_vlad@mail.ru>
2020-03-30 22:27:25 +0500

    Initial commit

---
+++ app/src/main/java/com/example/konovodov_hw_112/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.konovodov_hw_112;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
